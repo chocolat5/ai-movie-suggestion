1e26d992415da119575e14abc1577bac4c6a0ac3
chocolat5 <futatsugi.y@gmail.com>
2025-11-21 00:21:57 +0200

    feat: add Loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,25 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledSpinner = styled.div`
+  width: 24px;
+  height: 24px;
+  border: 3px solid rgba(255, 255, 255, 0.3);
+  border-top-color: var(--c-primary);
+  border-right-color: var(--c-primary);
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`;
+
+export const Loading = () => {
+  return <StyledSpinner />;
+};
