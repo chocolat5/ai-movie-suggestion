db6d89683b4e82354225708dc3987e2e6e139c66
chocolat5 <futatsugi.y@gmail.com>
2025-11-20 23:22:24 +0200

    chore: update style

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,35 @@ import styled from "@emotion/styled";
 
 import { ChatView } from "@/components/models/ChatView";
 import { StreamView } from "@/components/models/StreamView";
+import { Movie as MovieIcon } from "@/components/ui/Icons";
 import { TabBar } from "@/components/ui/TabBar";
 
 const StyledContainer = styled.section`
   display: flex;
   flex-direction: column;
-  max-width: 720px;
-  /* height: 100vh; */
+  max-width: var(--container-sm);
   margin: 0 auto;
-  padding: 6.4rem 3.2rem;
+  padding: var(--sp-3xl) var(--sp-xl);
 `;
 
 const StyledTitle = styled.h1`
-  font-size: 2.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--sp-sm);
+  font-size: var(--fs-xl);
   font-weight: 600;
   text-align: center;
+
+  svg {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+const StyledText = styled.p`
+  margin: var(--sp-md) 0 0;
+  font-size: var(--fs-md);
 `;
 
 function App() {
@@ -26,10 +40,15 @@ function App() {
 
   return (
     <StyledContainer>
-      <StyledTitle>AI Movie Suggestion</StyledTitle>
-      <p>
-        Type your favorite movies from all time or movies you liked recently
-      </p>
+      <StyledTitle>
+        <MovieIcon />
+        AI Movie Suggestion
+      </StyledTitle>
+      <StyledText>
+        Type your favorite movies from all time or movies you liked recently.
+        <br />
+        Get movies to watch next!
+      </StyledText>
       <div>
         <TabBar current={view} onViewChange={setView} />
         {view === "chat" ? <ChatView /> : <StreamView />}
